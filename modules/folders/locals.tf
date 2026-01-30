f1ad3caa87aff30577ac2293ff226fcf9dfbcce9
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-01-30 23:25:58 +0530

    changes in workflow

---
+++ modules/folders/locals.tf
@@ -0,0 +1,23 @@
+# All folders combined for easy reference
+locals {
+  all_folders = merge(
+    {
+      for key, folder in google_folder.parent_folders :
+      key => {
+        id           = folder.name
+        display_name = folder.display_name
+        parent       = folder.parent
+        type         = "parent"
+      }
+    },
+    {
+      for key, folder in google_folder.sub_folders :
+      key => {
+        id           = folder.name
+        display_name = folder.display_name
+        parent       = folder.parent
+        type         = "sub"
+      }
+    }
+  )
+}
